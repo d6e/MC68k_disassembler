a69143247222c6e175cf2e224df5feba2d553336
Danielle Jenkins <jenkinda@uw.edu>
2014-02-16 16:53:05 -0800

    Removed An as a source for all instructions that cannot have An as a source if the size is .B

--- make_test.py
+++ make_test.py
@@ -4,6 +4,7 @@
 size = ['.B', '.W', '.L']
 src = ['D1', 'A1', '(A1)', '#1', '(A1)+', '-(A1)','$10000000', '$1000']
 dest = ['D1','$10000000', '$1000']
+withoutAn = ['D1', '(A1)', '#1', '(A1)+', '-(A1)','$10000000', '$1000']
 
 
 class Instruction:
@@ -32,6 +33,48 @@ class Instruction:
 							else:
 								writeList.append(''.join([indent, self.opcode, size, ' ', s]))
 
+
+def initInstructionList():
+	# This is a list of all the instructions, with their exceptions. 
+	# 	I've added comments for exceptions for which I did not take into account
+	return [
+		Instruction('MOVE', src, dest, size, False), 
+		Instruction('MOVEQ', ['#1'], ['D1'], ['.L'], False), 
+		Instruction('MOVEM', ['D1/D2/D3', 'D1','$10000000', '$1000'], ['D1','$10000000', '$1000'], ['.W','.L']), 
+		Instruction('ADD', withoutAn, ['D1']), # Only for src: No 'An' if size is '.B' 
+		Instruction('ADDA', src, ['A1'], ['.W','.L'], False),
+		Instruction('ADDI', ['#1'], dest, size, False),
+		Instruction('SUB', withoutAn, ['D1']), # Only for src: No 'An' if size is '.B'
+		Instruction('SUBA', src, ['A1'], ['.W','.L'], False),
+		Instruction('SUBQ', ['#1'], dest, size, False), # Only for src: No 'An' if size is '.B'
+		Instruction('MULS', src, ['D1'], ['.W'], False),
+		Instruction('DIVU', src, ['D1'], ['.W'], False),
+		Instruction('LEA', src, ['A1'], ['.L'], False),
+		Instruction('CLR', src, [''], size, False),  # only one operand
+		Instruction('AND', src, ['D1']),
+		Instruction('ANDI', ['#1'], dest, size, False),
+		Instruction('EOR', ['D1'], dest, size, False),
+		Instruction('EORI', ['#1'], dest, size, False),
+		Instruction('ASR', ['D1'],['D2'], size, False),
+		Instruction('ASR', ['#1'],['D2'], size, False),
+		Instruction('ASR', src, [''], size, False),  # only one operand
+		Instruction('LSL', ['D1'], ['D2'], size, False),
+		Instruction('LSL', ['#1'], ['D2'], size, False),
+		Instruction('LSL', src, [''], size, False),
+		Instruction('ROL', ['D1'], ['D2'], size, False),
+		Instruction('ROR', ['D1'], ['D2'], size, False),
+		Instruction('BCHG', ['D1'], dest, ['.B','.L'], False), # 'Dn' can only be '.L' for dest, all others are '.B'
+		Instruction('BCHG', ['#1'], dest, ['.B','.L'], False), # 'Dn' can only be '.L' for dest, all others are '.B'
+		Instruction('CMP', withoutAn, ['D1'], size, False), # Only for src: No 'An' if size is '.B'
+		Instruction('CMPA', src, ['A1'], ['.W','.L'], False),
+		Instruction('CMPI', ['#1'], dest, size, False),
+		Instruction('BCC', src, dest, size, False, 'GO_TO_SR'),  # Only label
+		# Instruction('BGT', src, dest, size, False),
+		# Instruction('BLE', src, dest, size, False),
+		# Instruction('BVS', src, dest, size, False),
+		Instruction('JSR', src, [''], [''], False),
+		Instruction('RTS', [''], [''], [''], False),
+	]
 
 
 def getHeader():
@@ -65,48 +108,6 @@ def getFooter():
 		"*~Tab type~1~",
 		"*~Tab size~4~",
 	])
-
-def initInstructionList():
-	# This is a list of all the instructions, with their exceptions. 
-	# 	I've added comments for exceptions for which I did not take into account
-	return [
-		Instruction('MOVE', src, dest, size, False), 
-		Instruction('MOVEQ', ['#1'], ['D1'], ['.L'], False), 
-		Instruction('MOVEM', ['D1/D2/D3', 'D1','$10000000', '$1000'], ['D1','$10000000', '$1000'], ['.W','.L']), 
-		Instruction('ADD', src, ['D1']), # Only for src: No 'An' if size is '.B' 
-		Instruction('ADDA', src, ['A1'], ['.W','.L'], False),
-		Instruction('ADDI', ['#1'], dest, size, False),
-		Instruction('SUB', src, ['D1']), # Only for src: No 'An' if size is '.B'
-		Instruction('SUBA', src, ['A1'], ['.W','.L'], False),
-		Instruction('SUBQ', ['#1'], dest, size, False), # Only for src: No 'An' if size is '.B'
-		Instruction('MULS', src, ['D1'], ['.W'], False),
-		Instruction('DIVU', src, ['D1'], ['.W'], False),
-		Instruction('LEA', src, ['A1'], ['.L'], False),
-		Instruction('CLR', src, [''], size, False),  # only one operand
-		Instruction('AND', src, ['D1']),
-		Instruction('ANDI', ['#1'], dest, size, False),
-		Instruction('EOR', ['D1'], dest, size, False),
-		Instruction('EORI', ['#1'], dest, size, False),
-		Instruction('ASR', ['D1'],['D2'], size, False),
-		Instruction('ASR', ['#1'],['D2'], size, False),
-		Instruction('ASR', src, [''], size, False),  # only one operand
-		Instruction('LSL', ['D1'], ['D2'], size, False),
-		Instruction('LSL', ['#1'], ['D2'], size, False),
-		Instruction('LSL', src, [''], size, False),
-		Instruction('ROL', ['D1'], ['D2'], size, False),
-		Instruction('ROR', ['D1'], ['D2'], size, False),
-		Instruction('BCHG', ['D1'], dest, ['.B','.L'], False), # 'Dn' can only be '.L' for dest, all others are '.B'
-		Instruction('BCHG', ['#1'], dest, ['.B','.L'], False), # 'Dn' can only be '.L' for dest, all others are '.B'
-		Instruction('CMP', src, ['D1'], size, False), # Only for src: No 'An' if size is '.B'
-		Instruction('CMPA', src, ['A1'], ['.W','.L'], False),
-		Instruction('CMPI', ['#1'], dest, size, False),
-		Instruction('BCC', src, dest, size, False, 'GO_TO_SR'),  # Only label
-		# Instruction('BGT', src, dest, size, False),
-		# Instruction('BLE', src, dest, size, False),
-		# Instruction('BVS', src, dest, size, False),
-		Instruction('JSR', src, [''], [''], False),
-		Instruction('RTS', [''], [''], [''], False),
-	]
 
 if __name__ == '__main__':
 
